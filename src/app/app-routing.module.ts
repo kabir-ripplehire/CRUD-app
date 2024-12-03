f185dc69b2de950c7926d04016affb4b0e42a962
kabir <kabir.kalra@ripplehire.com>
2024-12-03 10:14:28 +0530

    Latest changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },  // Redirect from root to /products
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: '**', redirectTo: '/products' }  // Handle any undefined paths by redirecting to /products
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {OrdersModule} from "./orders/orders.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SiteFrameworkModule} from "./site-framework/site-framework.module";
+import {MatCardModule} from "@angular/material/card";
+
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OrdersModule,
+    BrowserAnimationsModule,
+    SiteFrameworkModule,
+    MatCardModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
